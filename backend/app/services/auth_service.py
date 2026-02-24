0470df7c3d6405aadb563248efc900ef1fe94736
Bouchta Mohamed <bouchtamohamed01@gmail.com>
2026-02-24 12:12:42 +0000

    Implement refresh token generation and validation in auth service

--- backend/app/services/auth_service.py
+++ backend/app/services/auth_service.py
@@ -1,11 +1,14 @@
 from sqlalchemy.orm import Session
 from app.models.user import Users
-from app.core.security import verify_password, create_access_token
+from app.core.security import verify_password, create_access_token,create_refresh_token
 
 def authenticate_user(db: Session, email: str, password: str):
     user = db.query(Users).filter(Users.email == email).first()
     if not user or not verify_password(password, user.hashed_password):
         return False
-    token = create_access_token(data={"sub": str(user.id), "role": user.role})
-    
-    return token
+    access_token = create_access_token(data={"sub": str(user.id), "role": user.role})
+    refresh_token = create_refresh_token(data={"sub": str(user.id), "role": user.role})
+    return {
+        "access_token": access_token,
+        "refresh_token": refresh_token
+    }
